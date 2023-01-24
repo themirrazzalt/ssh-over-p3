3b4043f9549eff773db8dcfed3204ed4081809aa
themirrazzalt <123411289+themirrazzalt@users.noreply.github.com>
2023-01-24 21:10:36 +0000

    Add files via upload

--- passwd.js
+++ passwd.js
@@ -6,7 +6,7 @@ const md5=require('md5');
 
 console.log('Enter a new password:');
 process.stdin.on('data', passwd => {
-    config.password=md5(passwd);
+    config.password=md5(passwd.slice(0,passwd.length-1));
     console.clear();
     fs.writeFileSync('./config.json',JSON.stringify(config));
     console.log('Password saved!');
